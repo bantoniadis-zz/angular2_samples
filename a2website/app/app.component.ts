0627c683250fd37ec3c172226a36590c2b013b1c
bill.antoniadis <bill.antoniadis@twinspires.com>
2017-01-12 11:04:04 -0500

    initial commit

---
+++ a2website/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>My first Angular App</h1><navbar></navbar>`,
+})
+export class AppComponent { }
---
+++ angular2-tour-of-heroes/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by bill.antoniadis on 8/22/16.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
