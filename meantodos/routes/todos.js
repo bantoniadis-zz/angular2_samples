4e1a0e4dd287bc129707ac69b2a2e7b43b3c247f
bill.antoniadis <bill.antoniadis@twinspires.com>
2017-01-12 13:26:41 -0500

    adding meantodos

--- meantodos/routes/todos.js
+++ meantodos/routes/todos.js
@@ -1,47 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://bill:bill@ds151028.mlab.com:51028/meantodos_bantoniadis_dev', ['todos']);
+var db = mongojs('mongodb://admin:admin@ds041516.mlab.com:41516/meantodos_brad_dev',['todos']);
 
 // Get Todos
-router.get('/todos', function(req, res, next) {
-    db.todos.find(function(err, todos) {
-        if (err) {
-            res.send(err);
+router.get('/todos', function(req, res, next){
+    db.todos.find(function(err, todos){
+        if(err){
+           res.send(err); 
+        } else {
+           res.json(todos);
         }
-        else {
-            res.json(todos);
-        }
-    })
+    });
 });
 
-// Get Single Todos
-router.get('/todo/:id', function(req, res, next) {
+// Get Single Todo
+router.get('/todo/:id', function(req, res, next){
     db.todos.findOne({
         _id: mongojs.ObjectId(req.params.id)
-    }, function(err, todo) {
-        if (err) {
-            res.send(err);
+    }, function(err, todo){
+        if(err){
+           res.send(err); 
+        } else {
+           res.json(todo);
         }
-        else {
-            res.json(todo);
-        }
-    })
+    });
 });
 
 // Save Todo
-router.post('/todo', function(req, res, next) {
+router.post('/todo', function(req, res, next){
     var todo = req.body;
-
-    if (!todo.text || !(todo.isCompleted + '')) {
+    if(!todo.text || !(todo.isCompleted + '')){
         res.status(400);
         res.json({
             "error": "Invalid Data"
         });
     } else {
-        db.todos.save(todo, function(err, result) {
-            if (err) {
-                res.send(err);
+        db.todos.save(todo, function(err, result){
+            if(err){
+                res.send(err); 
             } else {
                 res.json(result);
             }
@@ -50,19 +47,19 @@ router.post('/todo', function(req, res, next) {
 });
 
 // Update Todo
-router.put('/todo/:id', function(req, res, next) {
+router.put('/todo/:id', function(req, res, next){
     var todo = req.body;
     var updObj = {};
-
-    if (todo.isCompleted) {
-        updObj.isCompleted = todo.isCompleted;
+    
+    if(todo.isCompleted){
+       updObj.isCompleted = todo.isCompleted;
     }
-
-    if (todo.text) {
+    
+    if(todo.text){
         updObj.text = todo.text;
     }
-
-    if (!updObj) {
+    
+    if(!updObj){
         res.status(400);
         res.json({
             "error": "Invalid Data"
@@ -70,9 +67,9 @@ router.put('/todo/:id', function(req, res, next) {
     } else {
         db.todos.update({
             _id: mongojs.ObjectId(req.params.id)
-        }, updObj, {}, function(err, result) {
-            if (err) {
-                res.send(err);
+        },updObj, {}, function(err, result){
+            if(err){
+                res.send(err); 
             } else {
                 res.json(result);
             }
@@ -81,12 +78,12 @@ router.put('/todo/:id', function(req, res, next) {
 });
 
 // Delete Todo
-router.delete('/todo/:id', function(req, res, next) {
+router.delete('/todo/:id', function(req, res, next){
     db.todos.remove({
-            _id: mongojs.ObjectId(req.params.id)
-    }, '', function(err, result) {
-        if (err) {
-              res.send(err);
+        _id: mongojs.ObjectId(req.params.id)
+    },'', function(err, result){
+        if(err){
+            res.send(err); 
         } else {
             res.json(result);
         }
--- meantodos/server.js
+++ meantodos/server.js
@@ -12,17 +12,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-// Define where the static (i.e. client) files are located
 app.use(express.static(path.join(__dirname, 'client')));
 
-// Body Parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Routes
 app.use('/', index);
 app.use('/api/v1/', todos);
 
-app.listen(3000, function() {
+app.listen(3000, function(){
     console.log('Server started on port 3000...');
 });
