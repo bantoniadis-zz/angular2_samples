af737f70487576fad4b71389b77d669e7d84820d
bill.antoniadis <bill.antoniadis@twinspires.com>
2017-02-17 16:38:10 -0500

    Adding myweather app

---
+++ myweather/src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { WeatherPage } from '../weather/weather';
+import { SettingsPage } from '../settings/settings';
+import { ContactPage } from '../contact/contact';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  tab1Root: any = WeatherPage;
+  tab2Root: any = SettingsPage;
+
+  constructor() {
+
+  }
+}
